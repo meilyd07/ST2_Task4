402741daf5a2b8c861c3d1423392d8616d356aac
Hanna Rybakova <hanna.rybakowa@gmail.com>
2019-07-03 00:41:19 +0300

    allow user to show events

--- EventApp/EventApp/MainViewController.m
+++ EventApp/EventApp/MainViewController.m
@@ -8,9 +8,12 @@
 
 #import "MainViewController.h"
 #import "DateUtil.h"
+#import <EventKit/EventKit.h>
 
 @interface MainViewController ()
-
+@property (strong, nonatomic) EKEventStore *eventStore;
+@property (nonatomic) BOOL isAccessToEventStoreGranted;
+@property (strong, nonatomic) NSMutableArray *todoEvents;
 @end
 
 @implementation MainViewController
@@ -20,6 +23,52 @@
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 100, 200)];
     label.text = [DateUtil.sharedInstance getCurrentDate];
     [self.view addSubview:label];
+    [self updateAuthorizationStatusToAccessEventStore];
+}
+
+- (EKEventStore *)eventStore {
+    if (!_eventStore) {
+        _eventStore = [[EKEventStore alloc] init];
+    }
+    return _eventStore;
+}
+
+- (void)updateAuthorizationStatusToAccessEventStore {
+    EKAuthorizationStatus authorizationStatus = [EKEventStore authorizationStatusForEntityType:EKEntityTypeReminder];
+    
+    switch (authorizationStatus) {
+        case EKAuthorizationStatusDenied:
+        case EKAuthorizationStatusRestricted: {
+            self.isAccessToEventStoreGranted = NO;
+//            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Access Denied"
+//                                                                message:@"This app doesn't have access to your Reminders." delegate:nil
+//                                                      cancelButtonTitle:@"Dismiss" otherButtonTitles:nil];
+//            [alertView show];
+            //[self.tableView reloadData];
+            NSLog(@"This app doesn't have access to your Reminders.");
+            break;
+        }
+            
+            // 4
+        case EKAuthorizationStatusAuthorized:
+            self.isAccessToEventStoreGranted = YES;
+            NSLog(@"This app has access");
+            //[self.tableView reloadData];
+            break;
+            
+            // 5
+        case EKAuthorizationStatusNotDetermined: {
+            __weak MainViewController *weakSelf = self;
+            [self.eventStore requestAccessToEntityType:EKEntityTypeReminder
+                                            completion:^(BOOL granted, NSError *error) {
+                                                dispatch_async(dispatch_get_main_queue(), ^{
+                                                    weakSelf.isAccessToEventStoreGranted = granted;
+                                                    //[weakSelf.tableView reloadData];
+                                                });
+                                            }];
+            break;
+        }
+    }
 }
 
 
